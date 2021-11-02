28f3e131de0fcb2c4d0ad4236ef00b7b37d6ffc9
Luciano Figueroa <lumusitech@gmail.com>
2021-11-02 20:21:24 -0300

    [ADD] AuthRoute base styles added

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -3,7 +3,14 @@ import React from 'react'
 export const LoginScreen = () => {
     return (
         <div>
-            <h1>Login Screen</h1>
+            <h3>Login</h3>
+            <form>
+                <input type="text" name="email" placeholder="email" />
+                <input type="password" name="password" placeholder="password" />
+                <button type="submit">Login</button>
+                <hr />
+                google
+            </form>
         </div>
     )
 }
